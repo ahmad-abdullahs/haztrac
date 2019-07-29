02511165f553f7dc6c66f443499b626beea6986f
ahmad-abdullahs <ahmad.abdullah.khan.pm@gmail.com>
2019-07-30 03:40:45 +0500

    When adding EPA ID Number under svc related tab the number does not save (Fixed)

--- custom/Extension/modules/Accounts/Ext/Dependencies/SynoFieldMask_epa_id_c.php
+++ custom/Extension/modules/Accounts/Ext/Dependencies/SynoFieldMask_epa_id_c.php
@@ -1,32 +1,32 @@
 <?php
-$dependencies["Accounts"]["epa_id_c_synofieldmask"] = 
-array (
-  'hooks' => 
-  array (
-    0 => 'edit',
-    1 => 'save',
-  ),
-  'trigger' => 'true',
-  'triggerFields' => 
-  array (
-    0 => 'epa_id_c',
-  ),
-  'onload' => true,
-  'actions' => 
-  array (
-    0 => 
-    array (
-      'name' => 'SetSynoFieldMask',
-      'params' => 
-      array (
-        'target' => 'epa_id_c',
-        'label' => 'epa_id_c_label',
-        'value' => 
-        array (
-          'mask' => 'AAA999999999',
-          'greedy' => false,
-        ),
-      ),
+
+$dependencies["Accounts"]["ac_usepa_id_c_synofieldmask"] = array(
+    'hooks' =>
+    array(
+        0 => 'edit',
+        1 => 'save',
     ),
-  ),
+    'trigger' => 'true',
+    'triggerFields' =>
+    array(
+        0 => 'ac_usepa_id_c',
+    ),
+    'onload' => true,
+    'actions' =>
+    array(
+        0 =>
+        array(
+            'name' => 'SetSynoFieldMask',
+            'params' =>
+            array(
+                'target' => 'ac_usepa_id_c',
+                'label' => 'epa_id_c_label',
+                'value' =>
+                array(
+                    'mask' => 'AAA999999999',
+                    'greedy' => false,
+                ),
+            ),
+        ),
+    ),
 );
