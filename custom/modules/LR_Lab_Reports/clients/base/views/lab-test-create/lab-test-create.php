71525fba8b3b94a757e717769fe6e519e02303b5
ahmad-abdullahs <ahmad.abdullah.khan.pm@gmail.com>
2019-08-02 00:03:26 +0500

    lab report to display the fields selected in the multi select in the dashlet

--- custom/modules/LR_Lab_Reports/clients/base/views/lab-test-create/lab-test-create.php
+++ custom/modules/LR_Lab_Reports/clients/base/views/lab-test-create/lab-test-create.php
@@ -1,4 +1,5 @@
 <?php
+
 $viewdefs['LR_Lab_Reports']['base']['view']['llab-test-create'] = array(
     'label' => 'LBL_LAB_TESTS_DASHLET_VIEW',
     'description' => 'LBL_LAB_TESTS_DASHLET_VIEW_DESCRIPTION',
