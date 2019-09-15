46eb246e5b50b71d9d7f12f0c660e16273980d8f
root <root@sugarbear.pacoil.local>
2019-09-15 15:55:28 -0700

    Prod to git

--- custom/Extension/modules/sales_and_services/Ext/Vardefs/sugarfield_account_terms_c.php
+++ custom/Extension/modules/sales_and_services/Ext/Vardefs/sugarfield_account_terms_c.php
@@ -1,7 +1,7 @@
 <?php
- // created: 2019-06-17 16:45:12
-$dictionary['sales_and_services']['fields']['account_terms_c']['labelValue'] = 'Account Terms';
-$dictionary['sales_and_services']['fields']['account_terms_c']['dependency'] = '';
-$dictionary['sales_and_services']['fields']['account_terms_c']['visibility_grid'] = '';
-$dictionary['sales_and_services']['fields']['account_terms_c']['full_text_search']['boost'] = 1;
+ // created: 2019-09-14 17:23:25
+$dictionary['sales_and_services']['fields']['account_terms_c']['labelValue']='Account Terms';
+$dictionary['sales_and_services']['fields']['account_terms_c']['dependency']='';
+$dictionary['sales_and_services']['fields']['account_terms_c']['visibility_grid']='';
 
+ ?>
