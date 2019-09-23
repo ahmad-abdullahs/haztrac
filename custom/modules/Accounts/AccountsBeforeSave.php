6baf9829435d4358fe8be8d3c3472a91617725ca
ahmad-abdullahs <ahmad.abdullah.khan.pm@gmail.com>
2019-09-24 03:12:49 +0500

    Make account number auto generated

--- custom/modules/Accounts/AccountsBeforeSave.php
+++ custom/modules/Accounts/AccountsBeforeSave.php
@@ -1,10 +1,29 @@
 <?php
 
 class AccountsBeforeSave {
+
     public function generateUniqueAccountNumber($bean, $event, $arguments) {
-        if(!isset($bean->account_number_c) || empty($bean->account_number_c)){
-			$seven_digit_random_number = mt_rand(1000000, 9999999);
-			$bean->sample_id_number_c = $seven_digit_random_number;
-		}
+        if (!isset($bean->account_number_c) || empty($bean->account_number_c)) {
+            $seven_digit_random_number = mt_rand(1000000, 9999999);
+            $bean->sample_id_number_c = $seven_digit_random_number;
+        }
+
+        // Only assign account number when the record is created.
+        if (!$arguments['isUpdate']) {
+            $sq = new \SugarQuery();
+            $sq->from(BeanFactory::newBean('Accounts'), ['team_security' => false]);
+            $sq->select->fieldRaw('MAX(account_number_c)', 'account_number_c');
+
+            $account_number_c = $sq->getOne();
+            $GLOBALS['log']->fatal('Data : ' . $account_number_c);
+            $max_id = '';
+            if (empty($account_number_c)) {
+                $max_id = 'A17940';
+            } else {
+                $max_id = 'A' . (substr($account_number_c, 1) + 1);
+            }
+            $bean->account_number_c = $max_id;
+        }
     }
+
 }
--- custom/modules/RevenueLineItems/metadata/subpanels/Account_subpanel_revenuelineitems.php
+++ custom/modules/RevenueLineItems/metadata/subpanels/Account_subpanel_revenuelineitems.php
@@ -1,82 +1,89 @@
 <?php
+
 // created: 2019-09-23 13:06:59
-$subpanel_layout['list_fields'] = array (
-  'name' => 
-  array (
-    'vname' => 'LBL_LIST_NAME',
-    'widget_class' => 'SubPanelDetailViewLink',
-    'width' => 10,
-    'default' => true,
-  ),
-  'mft_part_num' => 
-  array (
-    'type' => 'varchar',
-    'vname' => 'LBL_MFT_PART_NUM',
-    'width' => 10,
-    'default' => true,
-  ),
-  'estimated_quantity_c' => 
-  array (
-    'type' => 'decimal',
-    'default' => true,
-    'vname' => 'LBL_ESTIMATED_QUANTITY',
-    'width' => 10,
-  ),
-  'discount_price' => 
-  array (
-    'type' => 'currency',
-    'related_fields' => 
-    array (
-      0 => 'currency_id',
-      1 => 'base_rate',
+$subpanel_layout['list_fields'] = array(
+    'name' =>
+    array(
+        'vname' => 'LBL_LIST_NAME',
+        'widget_class' => 'SubPanelDetailViewLink',
+        'width' => 10,
+        'default' => true,
+        'related_fields' =>
+        array(
+            0 => 'is_bundle_product_c',
+            1 => 'rli_as_template_c',
+            2 => 'sales_and_services_revenuelineitems_1_name',
+        ),
     ),
-    'vname' => 'LBL_DISCOUNT_PRICE',
-    'currency_format' => true,
-    'width' => 10,
-    'default' => true,
-  ),
-  'pricing_formula' => 
-  array (
-    'type' => 'varchar',
-    'vname' => 'LBL_PRICING_FORMULA',
-    'width' => 10,
-    'default' => true,
-  ),
-  'product_uom_c' => 
-  array (
-    'type' => 'enum',
-    'default' => true,
-    'vname' => 'LBL_PRODUCT_UOM',
-    'width' => 10,
-  ),
-  'related_rli_total_c' => 
-  array (
-    'type' => 'decimal',
-    'vname' => 'LBL_RELATED_RLI_TOTAL',
-    'width' => 10,
-    'default' => true,
-  ),
-  'product_vendor_c' => 
-  array (
-    'type' => 'relate',
-    'studio' => 'visible',
-    'vname' => 'LBL_PRODUCT_VENDOR',
-    'id' => 'V_VENDORS_ID_C',
-    'link' => true,
-    'width' => 10,
-    'default' => true,
-    'widget_class' => 'SubPanelDetailViewLink',
-    'target_module' => 'Accounts',
-    'target_record_key' => 'v_vendors_id_c',
-  ),
-  'discount_usdollar' => 
-  array (
-    'usage' => 'query_only',
-    'sortable' => false,
-  ),
-  'currency_id' => 
-  array (
-    'name' => 'currency_id',
-    'usage' => 'query_only',
-  ),
+    'mft_part_num' =>
+    array(
+        'type' => 'varchar',
+        'vname' => 'LBL_MFT_PART_NUM',
+        'width' => 10,
+        'default' => true,
+    ),
+    'estimated_quantity_c' =>
+    array(
+        'type' => 'decimal',
+        'default' => true,
+        'vname' => 'LBL_ESTIMATED_QUANTITY',
+        'width' => 10,
+    ),
+    'discount_price' =>
+    array(
+        'type' => 'currency',
+        'related_fields' =>
+        array(
+            0 => 'currency_id',
+            1 => 'base_rate',
+        ),
+        'vname' => 'LBL_DISCOUNT_PRICE',
+        'currency_format' => true,
+        'width' => 10,
+        'default' => true,
+    ),
+    'pricing_formula' =>
+    array(
+        'type' => 'varchar',
+        'vname' => 'LBL_PRICING_FORMULA',
+        'width' => 10,
+        'default' => true,
+    ),
+    'product_uom_c' =>
+    array(
+        'type' => 'enum',
+        'default' => true,
+        'vname' => 'LBL_PRODUCT_UOM',
+        'width' => 10,
+    ),
+    'related_rli_total_c' =>
+    array(
+        'type' => 'decimal',
+        'vname' => 'LBL_RELATED_RLI_TOTAL',
+        'width' => 10,
+        'default' => true,
+    ),
+    'product_vendor_c' =>
+    array(
+        'type' => 'relate',
+        'studio' => 'visible',
+        'vname' => 'LBL_PRODUCT_VENDOR',
+        'id' => 'V_VENDORS_ID_C',
+        'link' => true,
+        'width' => 10,
+        'default' => true,
+        'widget_class' => 'SubPanelDetailViewLink',
+        'target_module' => 'Accounts',
+        'target_record_key' => 'v_vendors_id_c',
+    ),
+    'discount_usdollar' =>
+    array(
+        'usage' => 'query_only',
+        'sortable' => false,
+    ),
+    'currency_id' =>
+    array(
+        'name' => 'currency_id',
+        'usage' => 'query_only',
+    ),
 );
