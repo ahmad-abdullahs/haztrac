e76724b32143d36a8a40d02cfc545e484713b8f9
ahmad-abdullahs <ahmad.abdullah.khan.pm@gmail.com>
2019-10-25 03:08:54 +0500

    Svc Days logic fixation and on time field removed from required list

--- custom/Extension/modules/sales_and_services/Ext/Vardefs/sugarfield_svc_days_c.php
+++ custom/Extension/modules/sales_and_services/Ext/Vardefs/sugarfield_svc_days_c.php
@@ -1,5 +1,5 @@
 <?php
- // created: 2019-10-25 01:48:57
+ // created: 2019-10-25 03:01:34
 $dictionary['sales_and_services']['fields']['svc_days_c']['duplicate_merge_dom_value']=0;
 $dictionary['sales_and_services']['fields']['svc_days_c']['labelValue']='Svc Days';
 $dictionary['sales_and_services']['fields']['svc_days_c']['full_text_search']=array (
@@ -8,7 +8,7 @@ $dictionary['sales_and_services']['fields']['svc_days_c']['full_text_search']=ar
   'searchable' => false,
 );
 $dictionary['sales_and_services']['fields']['svc_days_c']['calculated']='1';
-$dictionary['sales_and_services']['fields']['svc_days_c']['formula']='ifElse(equal($status_c,"Scheduled"),daysUntil($on_date_c),$svc_days_c)';
+$dictionary['sales_and_services']['fields']['svc_days_c']['formula']='ifElse(equal($status_c,"Scheduled"),floor(multiply(daysUntil($on_date_c),-1)),$svc_days_c)';
 $dictionary['sales_and_services']['fields']['svc_days_c']['enforced']='1';
 $dictionary['sales_and_services']['fields']['svc_days_c']['dependency']='';
 
