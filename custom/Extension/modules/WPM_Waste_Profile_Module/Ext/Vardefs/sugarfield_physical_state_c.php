ea04dc303003de5d0e9de63d1ce3d1dc40f8b256
ahmad-abdullahs <ahmad.abdullah.khan.pm@gmail.com>
2019-12-06 05:51:01 +0500

    Waste Profile View level changes and fields adding

--- custom/Extension/modules/WPM_Waste_Profile_Module/Ext/Vardefs/sugarfield_physical_state_c.php
+++ custom/Extension/modules/WPM_Waste_Profile_Module/Ext/Vardefs/sugarfield_physical_state_c.php
@@ -5,6 +5,15 @@ $dictionary['WPM_Waste_Profile_Module']['fields']['physical_state_c']['labelValu
 $dictionary['WPM_Waste_Profile_Module']['fields']['physical_state_c']['dependency'] = '';
 $dictionary['WPM_Waste_Profile_Module']['fields']['physical_state_c']['visibility_grid'] = '';
 
+$dictionary['WPM_Waste_Profile_Module']['fields']['physical_state_c']['dbType'] = 'enum';
+$dictionary['WPM_Waste_Profile_Module']['fields']['odor_c']['dbType'] = 'enum';
+$dictionary['WPM_Waste_Profile_Module']['fields']['flash_point_c']['dbType'] = 'enum';
+$dictionary['WPM_Waste_Profile_Module']['fields']['btu_c']['dbType'] = 'enum';
+$dictionary['WPM_Waste_Profile_Module']['fields']['ash_content_c']['dbType'] = 'enum';
+$dictionary['WPM_Waste_Profile_Module']['fields']['ph_c']['dbType'] = 'enum';
+$dictionary['WPM_Waste_Profile_Module']['fields']['viscosity_c']['dbType'] = 'enum';
+$dictionary['WPM_Waste_Profile_Module']['fields']['specific_gravity_c']['dbType'] = 'enum';
+$dictionary['WPM_Waste_Profile_Module']['fields']['wp_test_radio_c']['dbType'] = 'enum';
 $dictionary['WPM_Waste_Profile_Module']['fields']['physical_state_c']['type'] = 'radioenum';
 $dictionary['WPM_Waste_Profile_Module']['fields']['odor_c']['type'] = 'radioenum';
 $dictionary['WPM_Waste_Profile_Module']['fields']['flash_point_c']['type'] = 'radioenum';
