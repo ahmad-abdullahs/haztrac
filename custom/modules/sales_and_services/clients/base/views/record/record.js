ead3b1c57290bc75fd3e4059b0565c2e2be434fe
ahmad-abdullahs <ahmad.abdullah.khan.pm@gmail.com>
2019-12-28 07:09:03 +0500

    Add the Manifest Pdf Preview

--- custom/modules/sales_and_services/clients/base/views/record/record.js
+++ custom/modules/sales_and_services/clients/base/views/record/record.js
@@ -251,15 +251,56 @@
     },
 
     printPaperworkDrawer: function () {
-        app.drawer.open({
-            layout: 'print-paperwork',
-            context: {
-                create: true,
-                module: this.model.module || this.model.get('_module'),
-                model: this.model,
-            }
-        }, _.bind(function (context, taskmodel) {
-            // These are for code reference...
+        // app.drawer.open({
+        //     layout: 'print-paperwork',
+        //     context: {
+        //         create: true,
+        //         module: this.model.module || this.model.get('_module'),
+        //         model: this.model,
+        //     }
+        // }, _.bind(function (context, taskmodel) {
+        //     // These are for code reference...
+        // }, this));
+        this._downloadClicked();
+    },
+
+    _downloadClicked: function () {
+        app.bwc.login(null, _.bind(function () {
+            this._triggerDownload(this._buildDownloadLink());
         }, this));
+    },
+
+    _triggerDownload: function (url) {
+        var self = this;
+        app.api.fileDownload(url, {
+            success: function () {
+                app.drawer.open({
+                    layout: 'print-paperwork',
+                    context: {
+                        create: true,
+                        module: self.model.module || self.model.get('_module'),
+                        model: self.model,
+                    }
+                }, _.bind(function (context, taskmodel) {
+                    // These are for code reference...
+                }, self));
+            },
+            error: function (data) {
+                app.error.handleHttpError(data, {});
+            },
+        }, {
+            iframe: this.$el
+        });
+    },
+
+    _buildDownloadLink: function () {
+        var urlParams = $.param({
+            'action': 'manifest',
+            'module': this.model.get('_module'),
+            'record': this.model.get('id'),
+            'sugarpdf': 'pdfmanager',
+            'putToDir': true,
+        });
+        return '?' + urlParams;
     },
 })
