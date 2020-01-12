84694876dde49d302de9a36f925305fcce600078
ahmad-abdullahs <ahmad.abdullah.khan.pm@gmail.com>
2020-01-12 17:53:34 +0500

    Waste Profile Template designing and making Waste profile and waste profile template module according to that

--- custom/modules/WPM_Waste_Profile_Module/clients/base/views/create/create.js
+++ custom/modules/WPM_Waste_Profile_Module/clients/base/views/create/create.js
@@ -22,7 +22,8 @@
     initialize: function (options) {
         var self = this;
         this._super('initialize', [options]);
-        this.model.on('change:quest_is_usepa_hazardous_c', this.hideDependentFields, this);
+        this.model.on('change:quest_is_usepa_hazardous_c', this.hideUsepaHazardousDependentFields, this);
+        this.model.on('change:quest_animal_waste_c', this.hideAnimalWasteDependentFields, this);
         app.api.call('read', App.api.buildURL('getWasteProfileNum'), {}, {
             success: function (data) {
                 self.model.set('waste_profile_num_c', data);
@@ -33,13 +34,25 @@
         });
     },
 
-    hideDependentFields: function (model, fieldValue) {
+    hideUsepaHazardousDependentFields: function (model, fieldValue) {
         if (fieldValue == 'No') {
             this.$('[data-fieldname="quest_waste_from_facility_c"]').parent('div').parent('div').hide();
             this.$('[data-fieldname="quest_total_annual_benzene_c"]').parent('div').parent('div').hide();
         } else {
             this.$('[data-fieldname="quest_waste_from_facility_c"]').parent('div').parent('div').show();
             this.$('[data-fieldname="quest_total_annual_benzene_c"]').parent('div').parent('div').show();
+        }
+    },
+
+    hideAnimalWasteDependentFields: function (model, fieldValue) {
+        if (fieldValue == 'No' || fieldValue == 'NA') {
+            this.$('[data-fieldname="acknowledge_0"]').parent('div').parent('div').hide();
+            this.$('[data-fieldname="acknowledge_1_c"]').parent('div').parent('div').hide();
+            this.$('[data-fieldname="acknowledge_2_c"]').parent('div').parent('div').hide();
+        } else {
+            this.$('[data-fieldname="acknowledge_0"]').parent('div').parent('div').show();
+            this.$('[data-fieldname="acknowledge_1_c"]').parent('div').parent('div').show();
+            this.$('[data-fieldname="acknowledge_2_c"]').parent('div').parent('div').show();
         }
     },
 })
