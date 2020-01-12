6204b9ada79b1c6ce8035c0a2f2b17b540395f1a
ahmad-abdullahs <ahmad.abdullah.khan.pm@gmail.com>
2020-01-12 18:14:37 +0500

    Waste Profile Template designing and making Waste profile and waste profile template module according to that

--- custom/Extension/modules/WPM_Waste_Profile_Module/Ext/Vardefs/sugarfield_terms_and_conditions.php
+++ custom/Extension/modules/WPM_Waste_Profile_Module/Ext/Vardefs/sugarfield_terms_and_conditions.php
@@ -9,4 +9,5 @@ $dictionary['WPM_Waste_Profile_Module']['fields']['certificates'] = array(
     'reportable' => false,
     'duplicate_merge' => 'disabled',
     'audited' => true,
+    'default' => '094937b8-353d-11ea-9f45-000c29e77cbc',
 );
