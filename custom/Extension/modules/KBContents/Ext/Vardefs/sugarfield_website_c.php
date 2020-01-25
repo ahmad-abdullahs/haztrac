ba44e82d98ebbc8e4cacccf8cd5c0ad5dad927de
ahmad-abdullahs <ahmad.abdullah.khan.pm@gmail.com>
2020-01-25 17:19:47 +0500

    Adding iframe in Knowledge base module

---
+++ custom/Extension/modules/KBContents/Ext/Vardefs/sugarfield_website_c.php
@@ -0,0 +1,6 @@
+<?php
+ // created: 2020-01-25 17:15:04
+$dictionary['KBContent']['fields']['website_c']['labelValue']='Website';
+$dictionary['KBContent']['fields']['website_c']['dependency']='';
+
+ ?>
