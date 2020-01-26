29b8b66ddb738f35f2ab2566b10b49fc88fadf84
ahmad-abdullahs <ahmad.abdullah.khan.pm@gmail.com>
2020-01-26 21:53:22 +0500

    HPMB-104

--- custom/Extension/modules/WPM_Waste_Profile_Template/Ext/Vardefs/sugarfield_quest_greater_than_20organic_c.php
+++ custom/Extension/modules/WPM_Waste_Profile_Template/Ext/Vardefs/sugarfield_quest_greater_than_20organic_c.php
@@ -1,6 +1,6 @@
 <?php
  // created: 2019-12-09 20:32:50
-$dictionary['WPM_Waste_Profile_Template']['fields']['quest_greater_than_20organic_c']['labelValue']='DOES THE WASTE CONTAIN GREATER THAN 20% OF ORGANIC CONSTITUENTS WTTH A VAPOR PRESSURE >= .3KPA (.044 PSIA)?';
+$dictionary['WPM_Waste_Profile_Template']['fields']['quest_greater_than_20organic_c']['labelValue']='DOES THE WASTE CONTAIN GREATER THAN 20% OF ORGANIC CONSTITUENTS WITH A VAPOR PRESSURE >= .3KPA (.044 PSIA)?';
 $dictionary['WPM_Waste_Profile_Template']['fields']['quest_greater_than_20organic_c']['dependency']='';
 
  ?>
