6a68dc1542b55275dc03f8d89635fb8f27378845
ahmad-abdullahs <ahmad.abdullah.khan.pm@gmail.com>
2020-04-02 21:14:11 +0500

    Opportunity Related RevenueLineItems bug fix

--- custom/Extension/modules/Contracts/Ext/Vardefs/sugarfield_accounts_and_roles_widget.php
+++ custom/Extension/modules/Contracts/Ext/Vardefs/sugarfield_accounts_and_roles_widget.php
@@ -1,9 +1,9 @@
 <?php
 
 // created: 2019-10-31 21:29:25
-$dictionary['Contract']['fields']['accounts_and_roles_widget'] = array(
-    'name' => 'accounts_and_roles_widget',
-    'vname' => 'LBL_ACCOUNTS_AND_ROLES_WIDGET',
+$dictionary['Contract']['fields']['accounts_contact_role_widget'] = array(
+    'name' => 'accounts_contact_role_widget',
+    'vname' => 'LBL_ACCOUNTS_CONTACT_ROLE_WIDGET',
     'type' => 'text',
     'dbType' => 'text',
     'audited' => true,
