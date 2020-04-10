1841b73e7d3a5a9928e56fd7789f2c41ac0fbd81
ahmad-abdullahs <ahmad.abdullah.khan.pm@gmail.com>
2020-04-11 03:55:54 +0500

    Small change

--- custom/Extension/modules/HRM_Employee_Training/Ext/Language/en_us.custom_lang.php
+++ custom/Extension/modules/HRM_Employee_Training/Ext/Language/en_us.custom_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$mod_strings['LNK_NEW_RECORD_EMPLOYEE_INFO'] = 'Create Employee Info';
-$mod_strings['LNK_LIST_EMPLOYEE_INFO'] = 'View Employee Info';
+$mod_strings['LNK_NEW_RECORD_EMPLOYEE_INFO'] = 'Create Employee Contact';
+$mod_strings['LNK_LIST_EMPLOYEE_INFO'] = 'View Employee Contact';
