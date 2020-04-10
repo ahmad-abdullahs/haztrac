3afb2ef2ec3bab08ec0f7cfc121a47054c66648d
ahmad-abdullahs <ahmad.abdullah.khan.pm@gmail.com>
2020-04-10 19:06:45 +0500

    Add notification checkbox, list view column width, HR menu bar title

--- custom/modules/HT_Assets_and_Objects/clients/base/views/list/list.php
+++ custom/modules/HT_Assets_and_Objects/clients/base/views/list/list.php
@@ -16,57 +16,67 @@ $viewdefs['HT_Assets_and_Objects'] = array(
                         array(
                             0 =>
                             array(
-                                'name' => 'name',
-                                'type' => 'name',
-                                'label' => 'LBL_NAME',
-                                'default' => true,
-                                'enabled' => true,
-                                'link' => true,
-                                'width' => 'large',
-                            ),
-                            1 =>
-                            array(
-                                'name' => 'asset_type_c',
-                                'label' => 'LBL_ASSET_TYPE',
-                                'enabled' => true,
-                                'default' => true,
-                            ),
-                            2 =>
-                            array(
                                 'name' => 'asset_object_marking_no_c',
                                 'label' => 'LBL_ASSET_OBJECT_MARKING_NO',
                                 'enabled' => true,
                                 'default' => true,
+                                'width' => 'xsmall',
                             ),
-                            3 =>
-                            array(
-                                'name' => 'model_year_c',
-                                'label' => 'LBL_MODEL_YEAR',
-                                'enabled' => true,
-                                'default' => true,
-                            ),
-                            4 =>
-                            array(
-                                'name' => 'make_c',
-                                'label' => 'LBL_MAKE',
-                                'enabled' => true,
-                                'default' => true,
-                            ),
-                            5 =>
-                            array(
-                                'name' => 'model_c',
-                                'label' => 'LBL_MODEL',
-                                'enabled' => true,
-                                'default' => true,
-                            ),
-                            6 =>
+                            1 =>
                             array(
                                 'name' => 'object_no_c',
                                 'label' => 'LBL_OBJECT_NO',
                                 'enabled' => true,
                                 'default' => true,
                             ),
+                            2 =>
+                            array(
+                                'name' => 'asset_number',
+                                'label' => 'LBL_ASSET_NUMBER',
+                                'enabled' => true,
+                                'readonly' => true,
+                                'default' => true,
+                            ),
+                            3 =>
+                            array(
+                                'name' => 'name',
+                                'label' => 'LBL_NAME',
+                                'default' => true,
+                                'enabled' => true,
+                                'link' => true,
+                                'width' => 'medium',
+                            ),
+                            4 =>
+                            array(
+                                'name' => 'asset_type_c',
+                                'label' => 'LBL_ASSET_TYPE',
+                                'enabled' => true,
+                                'default' => true,
+                                'width' => 'small',
+                            ),
+                            5 =>
+                            array(
+                                'name' => 'model_year_c',
+                                'label' => 'LBL_MODEL_YEAR',
+                                'enabled' => true,
+                                'default' => true,
+                                'width' => 'xsmall',
+                            ),
+                            6 =>
+                            array(
+                                'name' => 'make_c',
+                                'label' => 'LBL_MAKE',
+                                'enabled' => true,
+                                'default' => true,
+                            ),
                             7 =>
+                            array(
+                                'name' => 'model_c',
+                                'label' => 'LBL_MODEL',
+                                'enabled' => true,
+                                'default' => true,
+                            ),
+                            8 =>
                             array(
                                 'name' => 'asset_location_c',
                                 'label' => 'LBL_ASSET_LOCATION',
@@ -76,42 +86,34 @@ $viewdefs['HT_Assets_and_Objects'] = array(
                                 'sortable' => false,
                                 'default' => true,
                             ),
-                            8 =>
+                            9 =>
                             array(
                                 'name' => 'team_name',
                                 'label' => 'LBL_TEAM',
-                                'default' => false,
+                                'default' => true,
                                 'enabled' => true,
-                                'width' => 'medium',
+                                'width' => 'small',
                             ),
-                            9 =>
+                            10 =>
+                            array(
+                                'name' => 'assigned_user_name',
+                                'label' => 'LBL_ASSIGNED_TO_NAME',
+                                'default' => true,
+                                'enabled' => true,
+                                'link' => true,
+                                'width' => 'small',
+                            ),
+                            11 =>
                             array(
                                 'name' => 'date_modified',
                                 'enabled' => true,
                                 'default' => false,
                             ),
-                            10 =>
+                            12 =>
                             array(
                                 'name' => 'date_entered',
                                 'enabled' => true,
                                 'default' => false,
-                            ),
-                            11 =>
-                            array(
-                                'name' => 'asset_number',
-                                'label' => 'LBL_ASSET_NUMBER',
-                                'enabled' => true,
-                                'readonly' => true,
-                                'default' => false,
-                            ),
-                            12 =>
-                            array(
-                                'name' => 'assigned_user_name',
-                                'label' => 'LBL_ASSIGNED_TO_NAME',
-                                'default' => false,
-                                'enabled' => true,
-                                'link' => true,
-                                'width' => 'medium',
                             ),
                         ),
                     ),
