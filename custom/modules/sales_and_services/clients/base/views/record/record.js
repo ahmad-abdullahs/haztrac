ed5f7e9d40ce3cf7255a97b4b758f9bb1eaf753e
ahmad-abdullahs <ahmad.abdullah.khan.pm@gmail.com>
2020-04-25 05:12:07 +0500

    Small change

--- custom/modules/sales_and_services/clients/base/views/record/record.js
+++ custom/modules/sales_and_services/clients/base/views/record/record.js
@@ -226,8 +226,8 @@
             return;
         }
 
-        // Call the trigger to notify the RevenueLineItem Subpanel to save all the items in the subpanel
-        this.context.trigger('save:full:subpanel:cstm');
+        // Unformat the Transporter field because this needs to be unformated through this way,
+        // Its a custom multirow field and its default unformat is not working.
         this.context.trigger('unformat:transporter:carrier');
 
         this._saveModel();
@@ -246,6 +246,76 @@
             this.toggleEdit(false);
             this.inlineEditMode = false;
         }
+    },
+
+    _saveModel: function () {
+        var options,
+                successCallback = _.bind(function () {
+                    // Call the trigger to notify the RevenueLineItem Subpanel to save all the items in the subpanel
+                    this.context.trigger('save:full:subpanel:cstm');
+
+                    // Loop through the visible subpanels and have them sync. This is to update any related
+                    // fields to the record that may have been changed on the server on save.
+                    _.each(this.context.children, function (child) {
+                        if (child.get('isSubpanel') && !child.get('hidden')) {
+                            if (child.get('collapsed')) {
+                                child.resetLoadFlag({recursive: false});
+                            } else {
+                                child.reloadData({recursive: false});
+                            }
+                        }
+                    });
+                    if (this.createMode) {
+                        app.navigate(this.context, this.model);
+                    } else if (!this.disposed && !app.acl.hasAccessToModel('edit', this.model)) {
+                        //re-render the view if the user does not have edit access after save.
+                        this.render();
+                    }
+                }, this);
+
+        //Call editable to turn off key and mouse events before fields are disposed (SP-1873)
+        this.turnOffEvents(this.fields);
+
+        options = {
+            showAlerts: true,
+            success: successCallback,
+            error: _.bind(function (model, error) {
+                if (error.status === 412 && !error.request.metadataRetry) {
+                    this.handleMetadataSyncError(error);
+                } else if (error.status === 409) {
+                    app.utils.resolve409Conflict(error, this.model, _.bind(function (model, isDatabaseData) {
+                        if (model) {
+                            if (isDatabaseData) {
+                                successCallback();
+                            } else {
+                                this._saveModel();
+                            }
+                        }
+                    }, this));
+                } else if (error.status === 403 || error.status === 404) {
+                    this.alerts.showNoAccessError.call(this);
+                } else {
+                    this.editClicked();
+                }
+            }, this),
+            lastModified: this.model.get('date_modified'),
+            viewed: true
+        };
+
+        // ensure view and field are sent as params so collection-type fields come back in the response to PUT requests
+        // (they're not sent unless specifically requested)
+        options.params = options.params || {};
+        if (this.context.has('dataView') && _.isString(this.context.get('dataView'))) {
+            options.params.view = this.context.get('dataView');
+        }
+
+        if (this.context.has('fields')) {
+            options.params.fields = this.context.get('fields').join(',');
+        }
+
+        options = _.extend({}, options, this.getCustomSaveOptions(options));
+
+        this.model.save({}, options);
     },
 
     isOnTopOfListView: function () {
