8cbcdd4714cf3f08ce0122c99313a55369846c4e
ahmad-abdullahs <ahmad.abdullah.khan.pm@gmail.com>
2020-07-02 05:04:12 +0500

    Manifest creation from subpanel under accounts module autopopulate the teams field in that.

---
+++ custom/modules/HT_Manifest/clients/base/views/panel-top/panel-top.js
@@ -0,0 +1,54 @@
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/Resources/Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+/**
+ * Header section for Subpanel layouts.
+ *
+ * @class View.Views.Base.PanelTopView
+ * @alias SUGAR.App.view.views.BasePanelTopView
+ * @extends View.View
+ */
+({
+    extendsFrom: 'PanelTopView',
+
+    initialize: function (options) {
+        this._super('initialize', [options]);
+    },
+
+    openCreateDrawer: function (module, link) {
+        link = link || this.context.get('link');
+        //FIXME: `this.context` should always be used - SC-2550
+        var context = (this.context.get('name') === 'tabbed-dashlet') ?
+                this.context : (this.context.parent || this.context);
+        var parentModel = context.get('model') || context.parent.get('model'),
+                model = this.createLinkModel(parentModel, link),
+                self = this;
+
+        // While menifest creation from the Accounts module set the Manifest team as it of Accounts
+        if (this.parentModule == 'Accounts') {
+            model.set('team_name', parentModel.get('team_name'));
+        }
+
+        app.drawer.open({
+            layout: 'create',
+            context: {
+                create: true,
+                module: model.module,
+                model: model
+            }
+        }, function (context, model) {
+            if (!model) {
+                return;
+            }
+
+            self.trigger('linked-model:create', model);
+        });
+    },
+})
