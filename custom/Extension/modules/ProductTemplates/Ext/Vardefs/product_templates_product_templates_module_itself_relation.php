094ff30c7d910a4063d3589aa04350e0b7150bb7
ahmad-abdullahs <ahmad.abdullah.khan.pm@gmail.com>
2022-02-15 00:22:28 +0500

    #HPMB-230 4-On Accounts module detail view, while creating a Sales and Service new Product Template bundles are not appearing in the dashboard tree, nor searchable.

--- custom/Extension/modules/ProductTemplates/Ext/Vardefs/product_templates_product_templates_module_itself_relation.php
+++ custom/Extension/modules/ProductTemplates/Ext/Vardefs/product_templates_product_templates_module_itself_relation.php
@@ -1,5 +1,62 @@
 <?php
 
+//// created: 2019-08-11 19:09:20
+//$dictionary["ProductTemplate"]["fields"]["product_templates_product_templates_1"] = array(
+//    'name' => 'product_templates_product_templates_1',
+//    'type' => 'link',
+//    'relationship' => 'product_templates_product_templates_1',
+//    'source' => 'non-db',
+//    'module' => 'ProductTemplates',
+//    'bean_name' => 'ProductTemplate',
+//    'vname' => 'LBL_PRODUCTTEMPLATES_PRODUCTTEMPLATES_1_FROM_PRODUCTTEMPLATES_L_TITLE',
+//    'id_name' => 'product_templates_product_templates_1product_templates_idb',
+////    'link-type' => 'many',
+////    'side' => 'left',
+//);
+//
+//$dictionary["ProductTemplate"]["fields"]["product_templates_product_templates_1_right"] = array(
+//    'name' => 'product_templates_product_templates_1_right',
+//    'type' => 'link',
+//    'relationship' => 'product_templates_product_templates_1',
+//    'source' => 'non-db',
+//    'module' => 'ProductTemplates',
+//    'bean_name' => 'ProductTemplate',
+//    'side' => 'right',
+//    'vname' => 'LBL_PRODUCTTEMPLATES_PRODUCTTEMPLATES_1_FROM_PRODUCTTEMPLATES_R_TITLE',
+//    'id_name' => 'product_templates_product_templates_1product_templates_ida',
+//    'link-type' => 'many',
+//);
+////
+////$dictionary["ProductTemplate"]["fields"]["product_templates_product_templates_1_name"] = array(
+////    'name' => 'product_templates_product_templates_1_name',
+////    'type' => 'relate',
+////    'source' => 'non-db',
+////    'vname' => 'LBL_PRODUCTTEMPLATES_PRODUCTTEMPLATES_1_FROM_PRODUCTTEMPLATES_L_TITLE',
+////    'save' => true,
+////    'id_name' => 'product_templates_product_templates_1product_templates_ida',
+////    'link' => 'product_templates_product_templates_1_right',
+////    'table' => 'product_templates',
+////    'module' => 'ProductTemplates',
+////    'rname' => 'name',
+////);
+////
+////$dictionary["ProductTemplate"]["fields"]["product_templates_product_templates_1product_templates_ida"] = array(
+////    'name' => 'product_templates_product_templates_1product_templates_ida',
+////    'type' => 'id',
+////    'source' => 'non-db',
+////    'vname' => 'LBL_PRODUCTTEMPLATES_PRODUCTTEMPLATES_1_FROM_PRODUCTTEMPLATES_R_TITLE_ID',
+////    'id_name' => 'product_templates_product_templates_1product_templates_ida',
+////    'link' => 'product_templates_product_templates_1_right',
+////    'table' => 'product_templates',
+////    'module' => 'ProductTemplates',
+////    'rname' => 'id',
+////    'reportable' => false,
+////    'side' => 'right',
+////    'massupdate' => false,
+////    'duplicate_merge' => 'disabled',
+////    'hideacl' => true,
+////);
+
 // created: 2019-08-11 19:09:20
 $dictionary["ProductTemplate"]["fields"]["product_templates_product_templates_1"] = array(
     'name' => 'product_templates_product_templates_1',
@@ -10,8 +67,8 @@ $dictionary["ProductTemplate"]["fields"]["product_templates_product_templates_1"
     'bean_name' => 'ProductTemplate',
     'vname' => 'LBL_PRODUCTTEMPLATES_PRODUCTTEMPLATES_1_FROM_PRODUCTTEMPLATES_L_TITLE',
     'id_name' => 'product_templates_product_templates_1product_templates_idb',
-//    'link-type' => 'many',
-//    'side' => 'left',
+    'link-type' => 'many',
+    'side' => 'left',
 );
 
 $dictionary["ProductTemplate"]["fields"]["product_templates_product_templates_1_right"] = array(
@@ -24,35 +81,34 @@ $dictionary["ProductTemplate"]["fields"]["product_templates_product_templates_1_
     'side' => 'right',
     'vname' => 'LBL_PRODUCTTEMPLATES_PRODUCTTEMPLATES_1_FROM_PRODUCTTEMPLATES_R_TITLE',
     'id_name' => 'product_templates_product_templates_1product_templates_ida',
-    'link-type' => 'many',
+    'link-type' => 'one',
 );
-//
-//$dictionary["ProductTemplate"]["fields"]["product_templates_product_templates_1_name"] = array(
-//    'name' => 'product_templates_product_templates_1_name',
-//    'type' => 'relate',
-//    'source' => 'non-db',
-//    'vname' => 'LBL_PRODUCTTEMPLATES_PRODUCTTEMPLATES_1_FROM_PRODUCTTEMPLATES_L_TITLE',
-//    'save' => true,
-//    'id_name' => 'product_templates_product_templates_1product_templates_ida',
-//    'link' => 'product_templates_product_templates_1_right',
-//    'table' => 'product_templates',
-//    'module' => 'ProductTemplates',
-//    'rname' => 'name',
-//);
-//
-//$dictionary["ProductTemplate"]["fields"]["product_templates_product_templates_1product_templates_ida"] = array(
-//    'name' => 'product_templates_product_templates_1product_templates_ida',
-//    'type' => 'id',
-//    'source' => 'non-db',
-//    'vname' => 'LBL_PRODUCTTEMPLATES_PRODUCTTEMPLATES_1_FROM_PRODUCTTEMPLATES_R_TITLE_ID',
-//    'id_name' => 'product_templates_product_templates_1product_templates_ida',
-//    'link' => 'product_templates_product_templates_1_right',
-//    'table' => 'product_templates',
-//    'module' => 'ProductTemplates',
-//    'rname' => 'id',
-//    'reportable' => false,
-//    'side' => 'right',
-//    'massupdate' => false,
-//    'duplicate_merge' => 'disabled',
-//    'hideacl' => true,
-//);
+$dictionary["ProductTemplate"]["fields"]["product_templates_product_templates_1_name"] = array(
+    'name' => 'product_templates_product_templates_1_name',
+    'type' => 'relate',
+    'source' => 'non-db',
+    'vname' => 'LBL_PRODUCTTEMPLATES_PRODUCTTEMPLATES_1_FROM_PRODUCTTEMPLATES_L_TITLE',
+    'save' => true,
+    'id_name' => 'product_templates_product_templates_1product_templates_ida',
+    'link' => 'product_templates_product_templates_1_right',
+    'table' => 'product_templates',
+    'module' => 'ProductTemplates',
+    'rname' => 'name',
+);
+
+$dictionary["ProductTemplate"]["fields"]["product_templates_product_templates_1product_templates_ida"] = array(
+    'name' => 'product_templates_product_templates_1product_templates_ida',
+    'type' => 'id',
+    'source' => 'non-db',
+    'vname' => 'LBL_PRODUCTTEMPLATES_PRODUCTTEMPLATES_1_FROM_PRODUCTTEMPLATES_R_TITLE_ID',
+    'id_name' => 'product_templates_product_templates_1product_templates_ida',
+    'link' => 'product_templates_product_templates_1_right',
+    'table' => 'product_templates',
+    'module' => 'ProductTemplates',
+    'rname' => 'id',
+    'reportable' => false,
+    'side' => 'right',
+    'massupdate' => false,
+    'duplicate_merge' => 'disabled',
+    'hideacl' => true,
+);
--- custom/metadata/product_templates_bundlesMetaData.php
+++ custom/metadata/product_templates_bundlesMetaData.php
@@ -1,8 +1,101 @@
 <?php
 
+//// created: 2019-08-11 19:09:20
+//$dictionary["product_templates_product_templates_1"] = array(
+//    'true_relationship_type' => 'many-to-many',
+//    'from_studio' => true,
+//    'relationships' =>
+//    array(
+//        'product_templates_product_templates_1' =>
+//        array(
+//            'lhs_module' => 'ProductTemplates',
+//            'lhs_table' => 'product_templates',
+//            'lhs_key' => 'id',
+//            'rhs_module' => 'ProductTemplates',
+//            'rhs_table' => 'product_templates',
+//            'rhs_key' => 'id',
+//            'relationship_type' => 'many-to-many',
+//            'join_table' => 'product_templates_product_templates_1_c',
+//            'join_key_lhs' => 'product_templates_product_templates_1product_templates_ida',
+//            'join_key_rhs' => 'product_templates_product_templates_1product_templates_idb',
+//        ),
+//    ),
+//    'table' => 'product_templates_product_templates_1_c',
+//    'fields' =>
+//    array(
+//        'id' =>
+//        array(
+//            'name' => 'id',
+//            'type' => 'id',
+//        ),
+//        'date_modified' =>
+//        array(
+//            'name' => 'date_modified',
+//            'type' => 'datetime',
+//        ),
+//        'deleted' =>
+//        array(
+//            'name' => 'deleted',
+//            'type' => 'bool',
+//            'default' => 0,
+//        ),
+//        'product_templates_product_templates_1product_templates_ida' =>
+//        array(
+//            'name' => 'product_templates_product_templates_1product_templates_ida',
+//            'type' => 'id',
+//        ),
+//        'product_templates_product_templates_1product_templates_idb' =>
+//        array(
+//            'name' => 'product_templates_product_templates_1product_templates_idb',
+//            'type' => 'id',
+//        ),
+//    ),
+//    'indices' =>
+//    array(
+//        0 =>
+//        array(
+//            'name' => 'idx_product_templates_product_templates_1_pk',
+//            'type' => 'primary',
+//            'fields' =>
+//            array(
+//                0 => 'id',
+//            ),
+//        ),
+//        1 =>
+//        array(
+//            'name' => 'idx_product_templates_product_templates_1_ida1_deleted',
+//            'type' => 'index',
+//            'fields' =>
+//            array(
+//                0 => 'product_templates_product_templates_1product_templates_ida',
+//                1 => 'deleted',
+//            ),
+//        ),
+//        2 =>
+//        array(
+//            'name' => 'idx_product_templates_product_templates_1_idb2_deleted',
+//            'type' => 'index',
+//            'fields' =>
+//            array(
+//                0 => 'product_templates_product_templates_1product_templates_idb',
+//                1 => 'deleted',
+//            ),
+//        ),
+//        3 =>
+//        array(
+//            'name' => 'product_templates_product_templates_1_alt',
+//            'type' => 'alternate_key',
+//            'fields' =>
+//            array(
+//                0 => 'product_templates_product_templates_1product_templates_idb',
+//            ),
+//        ),
+//    ),
+//);
+
 // created: 2019-08-11 19:09:20
 $dictionary["product_templates_product_templates_1"] = array(
-    'true_relationship_type' => 'many-to-many',
+    'true_relationship_type' => 'one-to-many',
     'from_studio' => true,
     'relationships' =>
     array(
