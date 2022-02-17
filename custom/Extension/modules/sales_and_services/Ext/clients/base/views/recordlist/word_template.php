88d9e4a668cdfb9562675e21cc74943ddb2b197b
ahmad-abdullahs <ahmad.abdullah.khan.pm@gmail.com>
2022-02-18 04:15:50 +0500

    #HPMB-231 5- Fix Performance Issue: On Sales and Service List View, there are additional calls going on for word template https://sugar.haztrac.com/rest/v11_4/word_templates.

--- custom/Extension/modules/sales_and_services/Ext/clients/base/views/recordlist/word_template.php
+++ custom/Extension/modules/sales_and_services/Ext/clients/base/views/recordlist/word_template.php
@@ -1,21 +1,25 @@
 <?php
-
-$viewdefs['sales_and_services']['base']['view']['recordlist']['rowactions']['actions'][] = array(
-    'type' => 'divider',
-);
-
-$viewdefs['sales_and_services']['base']['view']['recordlist']['rowactions']['actions'][] = array(
-    'type' => 'wordaction',
-    'name' => 'download-word',
-    'label' => 'LBL_DOWNLOAD_WORD_DOC',
-    'action' => 'download',
-    'acl_action' => 'view',
-);
-
-$viewdefs['sales_and_services']['base']['view']['recordlist']['rowactions']['actions'][] = array(
-    'type' => 'wordaction',
-    'name' => 'view-word',
-    'label' => 'LBL_VIEW_WORD_DOC',
-    'action' => 'email',
-    'acl_action' => 'view',
-);
+// Commented because:
+// 1- These buttons were not appearing on the list view.
+// 2- they were making 2 API calls per record on the list view (40 calls for default 20 record).
+// 3- It make the list view too slow, unstable and logout problem often.
+ 
+//$viewdefs['sales_and_services']['base']['view']['recordlist']['rowactions']['actions'][] = array(
+//    'type' => 'divider',
+//);
+//
+//$viewdefs['sales_and_services']['base']['view']['recordlist']['rowactions']['actions'][] = array(
+//    'type' => 'wordaction',
+//    'name' => 'download-word',
+//    'label' => 'LBL_DOWNLOAD_WORD_DOC',
+//    'action' => 'download',
+//    'acl_action' => 'view',
+//);
+//
+//$viewdefs['sales_and_services']['base']['view']['recordlist']['rowactions']['actions'][] = array(
+//    'type' => 'wordaction',
+//    'name' => 'view-word',
+//    'label' => 'LBL_VIEW_WORD_DOC',
+//    'action' => 'email',
+//    'acl_action' => 'view',
+//);
