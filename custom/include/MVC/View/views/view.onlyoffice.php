01f93ea29ed72e2246a5364645701e695d3f2299
ahmad-abdullahs <ahmad.abdullah.khan.pm@gmail.com>
2022-04-07 01:44:31 +0500

    Adding the RevenueLineItem functionality in the Document Manager printing

--- custom/include/MVC/View/views/view.onlyoffice.php
+++ custom/include/MVC/View/views/view.onlyoffice.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'custom/include/EntryPointScripts/onlyOfficeAjaxHelper.php';
+
 class ViewOnlyoffice extends SugarView {
 
     public $script = "";
@@ -13,13 +15,16 @@ class ViewOnlyoffice extends SugarView {
     function preDisplay() {
         global $db, $current_user, $timedate, $sugar_config;
 
+//        $e = new Exception();
+//        $GLOBALS['log']->fatal("stack trace : " . print_r($e->getTraceAsString(), 1));
+//        $GLOBALS['log']->fatal('Data : ' . print_r($_REQUEST, 1));
+
         $template_file_path = "{$sugar_config['onlyoffice_upload_dir1']}/{$_REQUEST['onlyoffice_template_id']}";
 
         $this->script = '';
         $this->script .= 'builder.OpenFile("' . $template_file_path . '.docx");' . PHP_EOL;
         $this->script .= 'var oDocument = Api.GetDocument();' . PHP_EOL;
 
-        require_once 'custom/include/EntryPointScripts/onlyOfficeAjaxHelper.php';
         $onlyOfficeAjaxHelperObj = new onlyOfficeAjaxHelper($this->bean->module_dir);
         $fieldsArrList = $onlyOfficeAjaxHelperObj->returnData();
         $data = $this->bean->toArray();
@@ -45,6 +50,12 @@ class ViewOnlyoffice extends SugarView {
                 }
             }
         }
+
+
+        $rliDataArr = $this->getRevenueLineItemsData();
+        $encodedDataArr = json_encode($rliDataArr);
+        $this->script .= "var rliDataArr = {$encodedDataArr};" . PHP_EOL;
+        $this->script .= $this->getRevenueLineItemScript();
 
 //        $saveFileName = "{$db->quoted($sugar_config['onlyoffice_upload_dir'] . "/" . $this->bean->name . ".docx")}";
 
@@ -142,6 +153,156 @@ class ViewOnlyoffice extends SugarView {
         $dataStr = str_replace(array("\r", "\n"), ' ', $dataStr);
         $dataStr = str_replace('"', '\"', $dataStr);
         return $dataStr;
+    }
+
+    function getRelatedRLIs() {
+        global $db;
+        $query = "SELECT 
+    revenue_line_items.id, revenue_line_items.name
+FROM
+    revenue_line_items
+        LEFT JOIN
+    revenue_line_items_cstm revenue_line_items_cstm ON revenue_line_items_cstm.id_c = revenue_line_items.id
+        INNER JOIN
+    sales_and_services_revenuelineitems_1_c sales_and_services_revenuelineitems_1 ON (revenue_line_items.id = sales_and_services_revenuelineitems_1.sales_and_services_revenuelineitems_1revenuelineitems_idb)
+        AND (sales_and_services_revenuelineitems_1.deleted = 0)
+        INNER JOIN
+    sales_and_services jt4_sales_and_services_revenuelineitems_1 ON (jt4_sales_and_services_revenuelineitems_1.id = sales_and_services_revenuelineitems_1.sales_and_services_revenuelineitems_1sales_and_services_ida)
+        AND (jt4_sales_and_services_revenuelineitems_1.deleted = 0)
+        AND (jt4_sales_and_services_revenuelineitems_1.id = '{$_REQUEST["record"]}')
+WHERE
+    revenue_line_items.deleted = 0
+ORDER BY revenue_line_items.line_number ASC , revenue_line_items.id ASC";
+        $result = $db->query($query, false);
+        return $result;
+    }
+
+    function getRevenueLineItemsData() {
+        global $db;
+        // Get related RLIs
+        $result = $this->getRelatedRLIs();
+
+        // Get fields List
+        $onlyOfficeAjaxHelperObj = new onlyOfficeAjaxHelper("RevenueLineItems");
+        $fieldsArrList = $onlyOfficeAjaxHelperObj->returnData();
+
+        // Data List
+        $rliDataArr = array();
+
+        $count = 1;
+        while ($row = $db->fetchByAssoc($result)) {
+            $rliBean = BeanFactory::getBean('RevenueLineItems', $row['id'], array('disable_row_level_security' => true));
+            $data = $rliBean->toArray();
+
+            foreach ($fieldsArrList['fieldsArr']['Fields'] as $key => $value) {
+                $data[$key] = $this->getCleanString($data[$key]);
+            }
+
+            $data['index'] = $count;
+            array_push($rliDataArr, $data);
+            $count ++;
+        }
+
+        return $rliDataArr;
+    }
+
+    function getRevenueLineItemScript() {
+        return ' // Paragraph Element for display
+        oParagraph = oDocument.GetElement(1);
+        // Desired table
+        var desiredTable, tempTable;
+        var fieldsList = [], fieldsListTemp = [];
+        // Flag Variables
+        var tableFound = false, cellFound = false;
+        var desiredTableRow = -1;
+
+        // Loop through all the elements to find the tables
+        for (var i = 0; i < oDocument.GetElementsCount(); i++) {
+            sClassType = oDocument.GetElement(i).GetClassType();
+
+            // If element is table
+            if (sClassType == "table") {
+                // Table Element
+                tempTable = oDocument.GetElement(i);
+
+                if (tempTable.GetRowsCount() > 0) {
+                    for (var j = 0; j < tempTable.GetRowsCount(); j++) {
+                        for (var k = 0; k < tempTable.GetRow(j).GetCellsCount(); k++) {
+                            oCell = tempTable.GetRow(j).GetCell(k);
+                            cellElement = oCell.GetContent().GetElement(0);
+                            cellText = cellElement.GetText();
+
+                            if (cellText.includes("[RevenueLineitem dynamic table]") && tableFound == false) {
+                                // This is the concerned table
+                                tableFound = true;
+                                break;
+                            }
+
+                            if (cellText.includes("{$fields.sales_and_services_revenuelineitems_1_c.") && tableFound == true) {
+                                fieldsList.push({[k]: cellText.trim()});
+                                fieldsListTemp.push(cellText.trim());
+
+                                desiredTable = tempTable;
+                                desiredTableRow = j;
+
+                                cellFound = true;
+                            } else if (tableFound == true && desiredTableRow == j) {
+                                fieldsList.push({[k]: cellText.trim()});
+                            }
+                        }
+                    }
+
+                    tableFound = false;
+                }
+
+                tempTable = null;
+            }
+        }
+
+        if (desiredTable) {
+            oDocument.SearchAndReplace({"searchString": "[RevenueLineitem dynamic table]", "replaceString": ""});
+
+            var additionalRowCounter = 0;
+
+            for (var j = 0; j < rliDataArr.length; j++) {
+
+                if (j != 0) {
+                    desiredTable.AddRow();
+                    additionalRowCounter++;
+                }
+
+                for (var k = 0; k < desiredTable.GetRow(desiredTableRow).GetCellsCount(); k++) {
+
+                    oCell = desiredTable.GetRow(desiredTableRow + additionalRowCounter).GetCell(k);
+                    cellElement = oCell.GetContent().GetElement(0);
+                    cellText = cellElement.GetText();
+
+                    if(cellText == ""){
+                        cellText = fieldsList[k][k] || "";
+                    }
+
+                    var fieldName = "";
+                    var pieces = cellText.split(".");
+                    // Greater than 1 is purposefully added, if the field name ends with dot e.g. {$fields.sales_and_services_revenuelineitems_1_c.Index}.
+                    // this check will handle
+                    if(pieces.length > 1){
+                        fieldName = pieces[2].replace("}", "");
+                        fieldName = fieldName.replace(".", "").toLowerCase();
+                    }
+
+                    var value = rliDataArr[j][fieldName];
+                    value = String(value);
+                    if(fieldName != ""){
+                        if (j == 0) {
+                            cellElement.RemoveAllElements();
+                            cellElement.AddText(value);
+                        } else {
+                            cellElement.AddText(value);
+                        }
+                    }
+                }
+            }
+        }' . PHP_EOL;
     }
 
 }
